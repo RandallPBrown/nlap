d33d3c01fa4da6a764d4f6a57f15d7e5144159b4
James Jezewski <james.r.jezewski@gmail.com>
2018-12-11 11:49:13 -0500

    Removed extra devise migration and added timestamps to user_agents table

--- db/migrate/20181207163601_rolify_create_roles.rb
+++ db/migrate/20181207163601_rolify_create_roles.rb
@@ -3,15 +3,15 @@ class RolifyCreateRoles < ActiveRecord::Migration[5.1]
     create_table(:roles) do |t|
       t.string :name
       t.references :resource, :polymorphic => true
-
       t.timestamps
     end
 
     create_table(:users_roles, :id => false) do |t|
       t.references :user
       t.references :role
+      t.timestamps
     end
-    
+
     add_index(:roles, [ :name, :resource_type, :resource_id ])
     add_index(:users_roles, [ :user_id, :role_id ])
   end
