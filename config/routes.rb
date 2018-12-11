282ce593af49a86f2b6bc1deef138bad86a15e4c
Peyton <randallpeytonbrown@gmail.com>
2018-12-11 08:47:56 -0600

    basic styling for login/sign up/edit user screens

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   resources :departments
   resources :agents
   devise_for :users, controllers: {
-        # sessions: 'users/sessions'
+        sessions: 'users/sessions',
         registrations: 'users/registrations'
       }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
