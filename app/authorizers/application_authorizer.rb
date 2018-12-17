a56f3ca7d38da7e31b88516ef1d6848c8f699d3d
Peyton <randallpeytonbrown@gmail.com>
2018-12-17 10:22:15 -0600

    trying removing authorizers

--- app/authorizers/application_authorizer.rb
+++ app/authorizers/application_authorizer.rb
@@ -1,5 +1,5 @@
 # Other authorizers should subclass this one
-class ApplicationAuthorizer < Authority::Authorizer
+# class ApplicationAuthorizer < Authority::Authorizer
 
   # Any class method from Authority::Authorizer that isn't overridden
   # will call its authorizer's default method.
@@ -7,10 +7,10 @@ class ApplicationAuthorizer < Authority::Authorizer
   # @param [Symbol] adjective; example: `:creatable`
   # @param [Object] user - whatever represents the current user in your app
   # @return [Boolean]
-  def self.default(adjective, user)
+  # def self.default(adjective, user)
     # 'Whitelist' strategy for security: anything not explicitly allowed is
     # considered forbidden.
     #user.has_role? :admin
-  end
+  # end
 
-end
+# end
