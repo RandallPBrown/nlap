3183c05a77016ad4694272c6b553a5208d30d791
Peyton <randallpeytonbrown@gmail.com>
2019-01-11 08:16:34 -0600

    debugging

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -17,6 +17,34 @@ class Agent < ApplicationRecord
     	joins(:occurrence, agent: :user)
   	}
 
+  scope :uww, -> {
+    joins(:writeup, :user).where("users.id = ?", current_user.id).count(:writeup_id)
+  }
+
+  scope :acd, -> {
+    joins(:occurrence, agent: :user).where("users.id = ?", current_user.id).group("occurrences.name").order("occurrences.name DESC").count("occurrences.name").values
+  }
+
+  scope :acl, -> {
+    joins(:occurrence, agent: :user).where("users.id = ?", current_user.id).group("occurrences.name").order("occurrences.name DESC").pluck("occurrences.name")
+  }
+
+  scope :ueae, -> {
+    joins(:occurrence, agent: :user).where("users.id = ?", current_user.id).where("occurrences.ovalue > ?", 0.5).count(:name)
+  }
+
+  scope :uete2, -> {
+    joins(:occurrence, agent: :user).where("users.id = ?", current_user.id).where("occurrences.name = ?", "Tardy").count(:name)
+  }
+
+  scope :uete, -> {
+    joins(:occurrence, agent: :user).where("users.id = ?", current_user.id).sum(:ovalue)
+  }
+
+  scope :ue, -> {
+    joins(:occurrence, agent: :user).where("users.id = ?", current_user.id).group(:id).order("entries.edate DESC")
+  }
+
 	def user_full_name
     	self.user.full_name
   end  
