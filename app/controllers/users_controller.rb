2a12d34a8e58b32827a03b03c146b858ee86fc3b
Peyton <randallpeytonbrown@gmail.com>
2019-01-18 16:00:32 -0600

    minor changes to user chart and new design started

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     @user_entry_tardy_effective = Entry.effective.occurrence_user.where("users.id = ?", current_user.id).uete2
     @user_entry_absent_effective = Entry.effective.occurrence_user.where("users.id = ?", current_user.id).ueae
   	@agent_chart_labels = Entry.effective.occurrence_user.where("users.id = ?", current_user.id).acl
-  	@agent_chart_data = Entry.effective.occurrence_user.acd
+  	@agent_chart_data = Entry.effective.occurrence_user.where("users.id = ?", current_user.id).acd
     @user_writeup_written = Dap.written.joins(:writeup, :user).where("users.id = ?", current_user.id).count(:writeup_id)
     @user_dap = Dap.includes(:writeup, :user).joins(:writeup, :user).group('writeups.id', 'users.id').where("users.id = ?", current_user.id).group(:id).order("daps.ddate DESC").paginate(page: params[:page], :per_page => 3)
   require 'will_paginate/array'
