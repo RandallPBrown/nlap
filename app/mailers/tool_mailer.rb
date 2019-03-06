81cc0c61f951b52e9ac2dc781812fac533ce5a7b
Peyton <randallpeytonbrown@gmail.com>
2019-03-06 15:15:49 -0600

    OTL Mailer test with variables added

--- app/mailers/tool_mailer.rb
+++ app/mailers/tool_mailer.rb
@@ -1,6 +1,6 @@
 class ToolMailer < ApplicationMailer
   def tool_email(tool)
     @tool = tool
-    mail(to: 'pbrown@newleafsc.net', subject: 'New Occurrence Logged')
+    mail(to: 'pbrown@newleafsc.net', subject: @tool.serviceorder)
   end
 end
