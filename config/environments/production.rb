39913cf02b0769f4064acde4f6e0a72cf34cc7d6
Peyton <rpeytonb@gmail.com>
2019-07-18 10:14:31 -0500

    mailgun is a pain in the ass

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,15 +64,14 @@ Rails.application.configure do
   config.action_mailer.asset_host = 'http://nlscap.net'
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { :host => 'mighty-mountain-66346.heroku.com' }
-  config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    :user_name            => ENV['MAILGUN_USERNAME'],
-    :password             => ENV['MAILGUN_PASSWORD'],
-    :address              => "smtp.mailgun.org",
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
+    :address              => "smtp.sendgrid.net",
     :port                 => 587,
     :enable_starttls_auto => true,
     :authentication       => :plain,
-    :domain               => "sandbox7fe0a9d0d8474760b4cebb2af84471a6.mailgun.org"
+    :domain               => "nlscap.net"
   }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
