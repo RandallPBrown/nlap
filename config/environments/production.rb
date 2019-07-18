e5852cac07e54d8a754f9c4cd884a2e7c6b9b2bd
Peyton <rpeytonb@gmail.com>
2019-07-18 10:08:54 -0500

    continued testing for mailgun conversion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
   config.action_mailer.asset_host = 'http://nlscap.net'
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { :host => 'mighty-mountain-66346.heroku.com' }
+  config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :user_name            => ENV['MAILGUN_USERNAME'],
     :password             => ENV['MAILGUN_PASSWORD'],
@@ -71,7 +72,7 @@ Rails.application.configure do
     :port                 => 587,
     :enable_starttls_auto => true,
     :authentication       => :plain,
-    :domain               => "nlscap.net"
+    :domain               => "sandbox7fe0a9d0d8474760b4cebb2af84471a6.mailgun.org"
   }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
