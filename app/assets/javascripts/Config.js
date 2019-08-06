fe1d4db87c77d17fe9e742212e2158a62088bbcc
Peyton <rpeytonb@gmail.com>
2019-08-06 11:32:34 -0500

    asdf

--- app/assets/javascripts/Config.js
+++ app/assets/javascripts/Config.js
@@ -1,2 +1,2 @@
-var webServiceUrl = "http://phonesrv-v.newleafsc.net:8098/";
+var webServiceUrl = "https://phonesrv-v.newleafsc.net:8098/";
 var webServiceUrlSecure = "https://phonesrv-v.newleafsc.net:8498/";
