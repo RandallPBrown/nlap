a8b3cdfb16efc19c1b6d44d8ad1aa9f136ffe4a1
Peyton <rpeytonb@gmail.com>
2019-12-13 11:18:58 -0600

    asdf

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,11 +8,10 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.precompile += %w( alphabetical_paginate.js )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( active_admin.coffee active_admin.scss )
+Rails.application.config.assets.precompile += %w( active_admin.js active_admin.scss )
 Rails.application.config.assets.precompile += %w( datatables.min.js datatables.min.css Config.js kendo.dataviz.min.js select.bootstrap4.min.css dataTables.select.min.js )
 Rails.application.config.assets.precompile += %w( slider.css user_dashboard.css dynamicuser.js papaparse.js js.cookie.js select.bootstrap4.min.js select.dataTables.min.css dataTables.select.min.js select.bootstrap4.js select.foundation.min.js select.dataTables.js nicEdit.js dtbuttons.min.js buttons.colVis.js dataTables.buttons.min.js papaparse.css demo.js demo.css network_maps.css pagination.css validator.css validator.js val.js val.css calendar.js )
