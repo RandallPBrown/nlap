edaa95ab26d18179d63c79d1af7dec8954a0d306
Peyton <rpeytonb@gmail.com>
2020-01-30 10:05:54 -0600

    trying asc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -93,15 +93,15 @@ class UsersController < ApplicationController
     @user_entry_tardy_effective = Entry.effective.occurrence_user.where("users.id = ?", current_user.id).uete2
     @user_entry_absent_effective = Entry.effective.occurrence_user.where("users.id = ?", current_user.id).ueae
   	
-    @agent_chart_labels_effective = Incentive.all.order(date: :desc).where(:date => 7.days.ago.to_date..Date.today.to_date.end_of_day).where("user_id = ?", helpers.current_user.id).map { |l| l.date.strftime("%m/%d/%Y") }.to_a;
-  	@agent_chart_data_effective = Incentive.all.order(date: :desc).where(:date => 7.days.ago.to_date..Date.today.to_date.end_of_day).where("user_id = ?", helpers.current_user.id).map { |l| l.uph }.to_a;
-    @agent_chart_data_effective_occupancy = Incentive.all.where(:date => 7.days.ago.to_date..Date.today.to_date.end_of_day).where("user_id = ?", helpers.current_user.id).map { |l| l.occupancy }.to_a;
-    @agent_chart_data_effective_aht = Incentive.all.where(:date => 7.days.ago.to_date..Date.today.to_date.end_of_day).where("user_id = ?", helpers.current_user.id).map { |l| l.aht }.to_a;
+    @agent_chart_labels_effective = Incentive.all.order(date: :asc).where(:date => 7.days.ago.to_date..Date.today.to_date.end_of_day).where("user_id = ?", helpers.current_user.id).map { |l| l.date.strftime("%m/%d/%Y") }.to_a;
+  	@agent_chart_data_effective = Incentive.all.order(date: :asc).where(:date => 7.days.ago.to_date..Date.today.to_date.end_of_day).where("user_id = ?", helpers.current_user.id).map { |l| l.uph }.to_a;
+    @agent_chart_data_effective_occupancy = Incentive.all.order(date: :asc).where(:date => 7.days.ago.to_date..Date.today.to_date.end_of_day).where("user_id = ?", helpers.current_user.id).map { |l| l.occupancy }.to_a;
+    @agent_chart_data_effective_aht = Incentive.all.order(date: :asc).where(:date => 7.days.ago.to_date..Date.today.to_date.end_of_day).where("user_id = ?", helpers.current_user.id).map { |l| l.aht }.to_a;
     
-    @agent_chart_labels_effective_month = Incentive.all.where(:date => Date.today.beginning_of_month..Date.today.to_date.end_of_day).where("user_id = ?", helpers.current_user.id).map { |l| l.date.strftime("%m/%d/%Y") }.to_a;
-    @agent_chart_data_effective_month = Incentive.all.where(:date => Date.today.beginning_of_month..Date.today.to_date.end_of_day).where("user_id = ?", helpers.current_user.id).map { |l| l.uph }.to_a;
-    @agent_chart_data_effective_occupancy_month = Incentive.all.where(:date => Date.today.beginning_of_month..Date.today.to_date.end_of_day).where("user_id = ?", helpers.current_user.id).map { |l| l.occupancy }.to_a;
-    @agent_chart_data_effective_aht_month = Incentive.all.where(:date => Date.today.beginning_of_month..Date.today.to_date.end_of_day).where("user_id = ?", helpers.current_user.id).map { |l| l.aht }.to_a;
+    @agent_chart_labels_effective_month = Incentive.all.order(date: :asc).where(:date => Date.today.beginning_of_month..Date.today.to_date.end_of_day).where("user_id = ?", helpers.current_user.id).map { |l| l.date.strftime("%m/%d/%Y") }.to_a;
+    @agent_chart_data_effective_month = Incentive.all.order(date: :asc).where(:date => Date.today.beginning_of_month..Date.today.to_date.end_of_day).where("user_id = ?", helpers.current_user.id).map { |l| l.uph }.to_a;
+    @agent_chart_data_effective_occupancy_month = Incentive.all.order(date: :asc).where(:date => Date.today.beginning_of_month..Date.today.to_date.end_of_day).where("user_id = ?", helpers.current_user.id).map { |l| l.occupancy }.to_a;
+    @agent_chart_data_effective_aht_month = Incentive.all.order(date: :asc).where(:date => Date.today.beginning_of_month..Date.today.to_date.end_of_day).where("user_id = ?", helpers.current_user.id).map { |l| l.aht }.to_a;
 
     @agent_chart_labels_total = Entry.all.occurrence_user.where("users.id = ?", current_user.id).acl
     @agent_chart_data_total = Entry.all.occurrence_user.where("users.id = ?", current_user.id).acd
